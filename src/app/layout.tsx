421a89d898495e340810bb3e84d7e4e13999a99e
PhamTinh <tinh1235a@gmail.com>
2026-05-11 17:30:33 +0900

    feat: initialize root layout with global styles, fonts, and AuthProvider wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
 import { Geist, Noto_Serif_JP } from "next/font/google";
+import { Suspense } from "react";
 
 
 const geistSans = Geist({
@@ -11,7 +12,7 @@ const geistSans = Geist({
 
 const notoSerifJP = Noto_Serif_JP({
   weight: ["400","700"],
-  subsets: ["latin"],
+  // subsets: ["latin"],
   variable: "--font-noto-serif-jp",
   preload: false,
   display: "swap",
@@ -21,9 +22,11 @@ export default function RootLayout({children,}: { children: React.ReactNode;}) {
   return (
     <html lang="en" className={`${geistSans.variable}  ${notoSerifJP.variable}`}>
       <body className="bg-background antialiased">
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <Suspense fallback={null}>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </Suspense>
       </body>
     </html>
   );
